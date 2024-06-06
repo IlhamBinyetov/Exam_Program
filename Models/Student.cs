abed1e5c2699f6d75bfadf3c73f694b884fec8d8
Ilham Binyetov <ilhamgb@code.edu.az>
2024-06-06 14:52:48 +0400

    Add project files.

---
+++ Models/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace Exam_Program.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Fin { get; set; }
+        public int StudentNumber { get; set; }
+        public int StudentClass { get; set; }
+    }
+}
